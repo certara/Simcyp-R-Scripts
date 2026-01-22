35bf4fb3a95f31fa9e9413c0245ce299fdd96488
Hiroshi <113444414+certara-hmomiji@users.noreply.github.com>
2026-01-22 10:24:21 +0000

    Update V25 files

--- Goodness Of Fit Plot using AUC and Cmax Ratio Data/Goodness Of Fit Plot- PK Ratio Data.R
+++ Goodness Of Fit Plot using AUC and Cmax Ratio Data/Goodness Of Fit Plot- PK Ratio Data.R
@@ -2,7 +2,7 @@
 #                                                                                              #
 #                             Goodness of Fit plot with Guest Criteria                         #
 #                                           Fluvoxamine                                        #
-#                                                                                              #   
+#                                                                                              #
 ################################################################################################
 #  Clear Global envrionment
 rm(list=ls())
@@ -10,69 +10,69 @@ rm(list=ls())
 # Load packages
 library("Simcyp")
 library("RSQLite")
-library("tidyverse") 
+library("tidyverse")
 
 
 
-# Set script to source file location 
+# Set script to source file location
 path_user <-Simcyp::ScriptLocation()
 setwd(path_user)
 
 
 # Here we check if "GOFObsandPredPKData.RData" file exists in your working folder the load it.
-# Otherwise, we create it.  
+# Otherwise, we create it.
 # -------------------------------------------------------------------------------------------
 if(file.exists("GOFObsandPredPKData.RData")){
-  
+
   # Load pre-saved data
   load("GOFObsandPredPKData.RData")
   message( "Loaded GOFObsandPredPKData.RData file" )
 } else {
-  
+
   # We will:
   # .load the Human Simcyp simulator
-  # .In a loop, run a simulation and Extract the summary stats of the Predicted 
+  # .In a loop, run a simulation and Extract the summary stats of the Predicted
   #  AUC and Cmax ratios of 4 studies. Store the results in an object called ForestData.
   # .Create the observed data and save into a dataframe for comparison .
-  
+
 
 #Initialise the system files path
-Simcyp::Initialise(species = SpeciesID$Human, verbose = FALSE) 
+Simcyp::Initialise(species = SpeciesID$Human, verbose = FALSE)
 
 
-# Enter workspace/study name manually. 
+# Enter workspace/study name manually.
 SimcypWksz<- c("CulmM2005_Caffeine_Fluvoxamine_DDI.wksz",
                "Christensen_caffeine_DDI.wksz",
                "Madsen_2001_FLUVOXDDI_75mg.wksz",
-               "Madsen_2001_FLUVOXDDI_150mg.wksz")  
+               "Madsen_2001_FLUVOXDDI_150mg.wksz")
 
 
 # Extract summary stats of Predicted AUC and Cmax ratio ---------------------------------------------
 # Empty vector for each workspace
-ForestData<- NULL  # 
+ForestData<- NULL  #
 
 
-for (Wks in 1:length(SimcypWksz)){ 
-  
+for (Wks in 1:length(SimcypWksz)){
+
   Workspace<- SimcypWksz[Wks]
-  SetWorkspace(file.path("V24 Workspaces/",Workspace))
-  
-  
+  SetWorkspace(file.path("Workspaces/",Workspace))
+
+
   ###### Run simulation and save to database
   #fs::path_ext_remove() "removes the last extension and returns the rest of the path".
-  DBfilename <- paste(fs::path_ext_remove(paste(Workspace)) ,".db",sep="")  
+  DBfilename <- paste(fs::path_ext_remove(paste(Workspace)) ,".db",sep="")
   DBfilepath <- file.path(path_user, DBfilename) # File path to save the database results to
   Simulate(database = DBfilepath)
   conn <- RSQLite::dbConnect(SQLite(),DBfilepath)
-  
+
   # Get the AUC and Cmax ratio for the last dose and for the full population
   PredRatio<- GetForestData_DB(Alpha = 0.05, Upper = 95, Lower = 5, conn, Last_Dose = TRUE, AUC_Type = "AUCt")
-  
+
   ForestData<- data.frame(rbind(ForestData, PredRatio))
-  
+
   RSQLite::dbDisconnect(conn)      #Detach database connection
   message( paste( SimcypWksz[Wks], "is now complete :) "  ) )
-  
+
 }
 
 
@@ -89,18 +89,18 @@ observed.Cmaxratio <- c(1.4,2.90,NA,NA)
 observed.AUCratio <- c(13.71,6.14,1.25,1.93)
 
 # Compound Name; the order matters!
-# make sure they are grouped by compound names. 
-Compound <- c("Caffeine",      
+# make sure they are grouped by compound names.
+Compound <- c("Caffeine",
               "Caffeine",
               "Tolbutamide",
               "Tolbutamide")
 
 # Prep data for GOF plot. combine observed and Predicted data
-GOFDataCmax<- data.frame(Compound=Compound, Observed= observed.Cmaxratio , 
-           Predicted= Predicted.CmaxRatio$Mean ) 
+GOFDataCmax<- data.frame(Compound=Compound, Observed= observed.Cmaxratio ,
+           Predicted= Predicted.CmaxRatio$Mean )
 
-GOFDataAUC<- data.frame(Compound=Compound, Observed= observed.AUCratio , 
-                         Predicted= Predicted.AUCRatio$Mean ) 
+GOFDataAUC<- data.frame(Compound=Compound, Observed= observed.AUCratio ,
+                         Predicted= Predicted.AUCRatio$Mean )
 
 save(GOFDataCmax, GOFDataAUC, file= "GOFObsandPredPKData.RData")
 }
@@ -109,31 +109,31 @@ save(GOFDataCmax, GOFDataAUC, file= "GOFObsandPredPKData.RData")
 
 # Guest criteria parameters
 GuestData<- function(breaks){
-  
+
   xLow <- breaks[1]
   xHigh <- breaks[length(breaks)]
-  
+
   stepSize <- xLow/10
-  
+
   RobsLow <- seq(xLow,1,stepSize)
   LimXlow <- (1+2*((1/RobsLow)-1))/(1/RobsLow)
-  
+
   LimLowUpper <- RobsLow*LimXlow
   LimLowLower <- RobsLow/LimXlow
-  
+
   RobsHigh <- seq(from=1,to=xHigh,by=stepSize)
   LimXhigh <- (1+2*(RobsHigh-1))/RobsHigh
-  
+
   LimHighUpper <- RobsHigh*LimXhigh
   LimHighLower <- RobsHigh/LimXhigh
-  
+
   Robs <- c(RobsLow,RobsHigh)
   LimUpper <- c(LimLowUpper,LimHighUpper)
   LimLower <- c(LimLowLower, LimHighLower)
-  
+
   guestUpper <- data.frame(Robs, LimUpper)
   guestLower <- data.frame(Robs, LimLower)
-  
+
   return(list( Upper= guestUpper, Lower=guestLower))
 }
 
@@ -155,24 +155,24 @@ Limits<- c(
 # ---------------------------GOF and Guest plot -------------------------
 
 Plot1 <- ggplot(data=GOFData, aes(x=Observed, y=Predicted)) + #,xaxs="i", yaxs="i"
-  geom_point(aes(color=factor(Compound)), size=3) + 
+  geom_point(aes(color=factor(Compound)), size=3) +
   geom_line(data=GuestData(breaks)$Upper, aes(x=Robs,y=LimUpper), color="red") +
   geom_line(data=GuestData(breaks)$Lower, aes(x=Robs,y=LimLower), color="red") +
-  geom_abline(slope=1,intercept=0) +  # Line of identity 
+  geom_abline(slope=1,intercept=0) +  # Line of identity
   # geom_ribbon(data=Fold_Error_Guest,aes(x=x,ymin=ymin1,ymax=ymax1),alpha=0.5,fill="grey") #  1.25 fold deviation
   geom_abline(slope=1.25,intercept=0,color="gray") +  #  1.25 fold deviation
   geom_abline(slope=1/1.25,intercept=0,color="gray") +   # 1.25 fold deviation
-  geom_abline(slope=2,intercept=0,color="black",linetype="dashed") +  # 2 fold deviation 
-  geom_abline(slope=1/2,intercept=0,color="black",linetype="dashed") +  # 2 fold deviation 
+  geom_abline(slope=2,intercept=0,color="black",linetype="dashed") +  # 2 fold deviation
+  geom_abline(slope=1/2,intercept=0,color="black",linetype="dashed") +  # 2 fold deviation
   coord_trans(x="log10", y="log10") +
   scale_x_continuous(expand=c(0,0), name=xLabelName, breaks=breaks, labels=breaks, limits=Limits) +
   scale_y_continuous(expand=c(0,0), name=yLabelName, breaks=breaks, labels=breaks, limits=Limits) +
   # limits=c(breaks[[1]], breaks[[length(breaks)]])
-  theme(aspect.ratio = 1, 
+  theme(aspect.ratio = 1,
         # axis.line = element_line(size = 0.3, colour = "black", linetype=1),
-        # axis.ticks = element_line(size = 1, color="black"), 
-        plot.title = element_text(color = "black", size = 12, face = "bold", hjust = 0.5), 
-        panel.border = element_rect(color = "black", fill = NA,size = 1))+ 
+        # axis.ticks = element_line(size = 1, color="black"),
+        plot.title = element_text(color = "black", size = 12, face = "bold", hjust = 0.5),
+        panel.border = element_rect(color = "black", fill = NA,size = 1))+
   ggtitle(GraphTitle)+
   theme(  # Legend position
     panel.background = element_rect(fill='transparent'), #transparent panel bg
@@ -182,7 +182,7 @@ Plot1 <- ggplot(data=GOFData, aes(x=Observed, y=Predicted)) + #,xaxs="i", yaxs="
     legend.position = c(0, 1), #legend.position = "bottom"
     legend.justification = c("left", "top"),
     legend.box.just = "left",
-    legend.margin = margin(6, 6, 6, 6), 
+    legend.margin = margin(6, 6, 6, 6),
     legend.background = element_rect(fill='transparent'),
     legend.key=element_blank()) +
   labs(color=NULL)   # remove legend title
@@ -193,4 +193,4 @@ print(Plot1)
 ggsave("GOF-Fluvoxamine-AUCRatio.pdf")
 
 
-# END 
+# END
