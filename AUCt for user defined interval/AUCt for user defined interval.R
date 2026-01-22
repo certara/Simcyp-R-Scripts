11edd184ce72c0047e7ce0b0208b3593ec7e7a53
Hiroshi <113444414+certara-hmomiji@users.noreply.github.com>
2026-01-22 11:07:11 +0000

    Update V25 files

--- AUCt for user defined interval/AUCt for user defined interval.R
+++ AUCt for user defined interval/AUCt for user defined interval.R
@@ -5,12 +5,12 @@ library(tidyverse)
 library(pracma)
 
 
-# Set script to source file location 
+# Set script to source file location
 path_user <-Simcyp::ScriptLocation()
 setwd(path_user)
 
-# Initialise simcyp engine. 
-Simcyp::Initialise("C://Program files//Simcyp simulator v24//screens//systemfiles", 24)
+# Initialise simcyp engine.
+Simcyp::Initialise("C://Program files//Simcyp simulator v25//screens//systemfiles", 25)
 
 
 Simcyp::SetWorkspace("Mikus_2020_150mgCOBI_midazolam-DDI.wksz")
@@ -24,67 +24,67 @@ auc_values <- array(data.frame(), dim = nTrials)
 cmax_values <- array(data.frame(), dim = nTrials)
 
 
-# For each individual in the population, extract the Systemic concentration time profile. 
+# For each individual in the population, extract the Systemic concentration time profile.
 for (i in 1:nPop) {
    nTrial <- GetIndividualValue_DB(i, "idGroupNo", conn = conn)
-   
+
    # substrate conc in plasma in the absence of drug interaction for individuals
-   Profile <- GetProfile_DB(ProfileID$CsysFull , individual=i, compound=CompoundID$Substrate, conn, inhibition = FALSE) 
+   Profile <- GetProfile_DB(ProfileID$CsysFull , individual=i, compound=CompoundID$Substrate, conn, inhibition = FALSE)
    # substrate conc in plasma in the presence of drug interaction for individuals
-   Profile_inh <- GetProfile_DB(ProfileID$CsysFull , individual=i, compound=CompoundID$Substrate, conn, inhibition = TRUE) 
-  
-   
-   # Calculate the AUC within the time interval specified above of Csys WITHOUT Interaction . 
+   Profile_inh <- GetProfile_DB(ProfileID$CsysFull , individual=i, compound=CompoundID$Substrate, conn, inhibition = TRUE)
+
+
+   # Calculate the AUC within the time interval specified above of Csys WITHOUT Interaction .
    Time <- Simcyp::GetProfile_DB(ProfileID$nTimeSubFull, compound=-1 , inhibition = FALSE, conn, individual = 1) # Time data
    df1 <- data.frame(sapply(Profile,c))
    Profile1 <- data.frame(Time, df1)
-   
-   # Time interval 
-   From <- 26.5 
+
+   # Time interval
+   From <- 26.5
    To <- 28.5
-   
+
    prof1 <- Profile1 %>% filter(between(Time, From, To))
    Time <-  prof1$Time
    prof1 <- prof1[ -c(1) ]
    AUCprof1 <- as.list(as.data.frame(prof1))
-   
+
    # AUC Calculation
    for(i in 1:length(prof1)){
       AUCprof1[[i]] <- trapz(Time, AUCprof1[[i]])
    }
-   
-   
-   
-   # Calculate the AUC within the time interval specified above of Csys WITH Interaction . 
+
+
+
+   # Calculate the AUC within the time interval specified above of Csys WITH Interaction .
    Time <- Simcyp::GetProfile_DB(ProfileID$nTimeSubFull, compound=-1 , inhibition = FALSE, conn, individual = 1) # Time data
    df2 <- data.frame(sapply(Profile_inh,c))
    Profile2 <- data.frame(Time, df2)
-   
+
    prof2 <- Profile2 %>% filter(between(Time, From, To))
    Time <-  prof2$Time
    prof2 <- prof2[ -c(1) ]
    AUCprof2 <- as.list(as.data.frame(prof2))
-   
+
    # AUC Calculation
    for(i in 1:length(prof2)){
       AUCprof2[[i]] <- trapz(Time, AUCprof2[[i]])
-   }    
-  
-   
-   # AUC Ratio 
+   }
+
+
+   # AUC Ratio
    auc_ratio <- mapply(FUN = `/`, AUCprof2, AUCprof1, SIMPLIFY = FALSE)
    auc_ratio <- t(data.frame(auc_ratio))
    auc_values[[nTrial]] <- c(auc_values[[nTrial]], auc_ratio)
-   
-   # Cmax Ratio 
+
+   # Cmax Ratio
    colMax <- function(data) sapply(data, max, na.rm = TRUE)
    Cmax1 <- data.frame(colMax(prof1))
    Cmax2 <- data.frame(colMax(prof2))
    cmax_ratio <- Cmax2/Cmax1
    cmax_ratio <- unlist(cmax_ratio)
-   
+
    cmax_values[[nTrial]] <- c(cmax_values[[nTrial]], cmax_ratio)
-   
+
 }
 
 # Statistics Calculation
@@ -105,13 +105,13 @@ predicted.cmax <- vector()
 predicted.AUCratio <- vector()
 diffCmax <- c(diffCmax, sprintf("%0.2f", cmax_mean/observed.cmax))
 diffAUC <- c(diffAUC, sprintf("%0.2f", AUC_mean/observed.AUCratio))
-predicted.cmax <- c(predicted.cmax, sprintf("%0.2f( %0.2f to %0.2f)", 
+predicted.cmax <- c(predicted.cmax, sprintf("%0.2f( %0.2f to %0.2f)",
                                             cmax_mean, cmax_low, cmax_high))
-predicted.AUCratio <- c(predicted.AUCratio, sprintf("%0.2f( %0.2f to %0.2f)", 
+predicted.AUCratio <- c(predicted.AUCratio, sprintf("%0.2f( %0.2f to %0.2f)",
                                                     AUC_mean, AUC_low, AUC_high))
 RSQLite::dbDisconnect(conn)
 
-df1 <- data.frame(name, observed.cmax, observed.AUCratio, 
+df1 <- data.frame(name, observed.cmax, observed.AUCratio,
                   predicted.cmax, predicted.AUCratio, diffCmax, diffAUC)
 colnames(df1) <- c("Study", "Cmax Ratio", "AUC Ratio", "Cmax Ratio", "AUC Ratio", "Cmax Ratio", "AUC ratio")
 
